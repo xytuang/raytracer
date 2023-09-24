925680e10691bb6eda421cef9e065bdc7a38b132
Xiang Yu Tuang <xiangyutuang@gmail.com>
2023-09-24 14:39:14 -0400

    created circle

--- main.cc
+++ main.cc
@@ -1,13 +1,49 @@
 #include <iostream>
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(),r.direction());
+    auto b = 2 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
+color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r)){
+        return color(1,0,0);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
 int main() {
+    auto aspect_ratio = 16.0/9.0;
+    int image_width = 400;
+    int image_height = static_cast<int>(image_width/aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
 
-    // Image
+    //camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height* (static_cast<double>(image_width)/image_height);
+    auto camera_center = point3(0,0,0);
 
-    int image_width = 256;
-    int image_height = 256;
+    //viewport vectors
+    auto viewport_u = vec3(viewport_width, 0,0);
+    auto viewport_v = vec3(0,-viewport_height,0);
+
+    //gap between pixels
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    //calculate upper left pixel
+    auto viewport_upper_left = camera_center
+                             - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
 
@@ -16,7 +52,11 @@ int main() {
     for (int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            auto pixel_center = pixel00_loc + i * viewport_u + j * viewport_v;
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
